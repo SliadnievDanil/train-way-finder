a16e217b5ed07fdf3c3cf8cc7e5b70d56616105b
SliadnievDanil <sladnevdanil78@gmail.com>
2024-11-03 14:03:23 +0000

    accounts: created accounts app

---
+++ accounts/forms.py
@@ -0,0 +1,56 @@
+from django import forms
+from django.contrib.auth import get_user_model, authenticate
+
+User = get_user_model()
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(label='username', widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter username'
+    }))
+    password = forms.CharField(label='password', widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter password'
+    }))
+
+    def clean(self, *args, **kwargs):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        if username and password:
+            qs = User.objects.filter(username=username)
+            if not qs.exists():
+                raise forms.ValidationError('There is no such user')
+            if not qs[0].check_password(password):
+                raise forms.ValidationError('Incorrect password')
+            user = authenticate(username=username, password=password)
+            if not user:
+                raise forms.ValidationError('This user is inactive')
+        return super().clean(*args, **kwargs)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    username = forms.CharField(label='username', widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter username'
+    }))
+    password = forms.CharField(label='password', widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter password'
+    }))
+    password2 = forms.CharField(label='password', widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter password'
+    }))
+
+    class Meta:
+        model = User
+        fields = ('username', )
+
+    def clean_password2(self):
+        data = self.cleaned_data
+        if data['password'] != data['password2']:
+            raise forms.ValidationError('Passwords must match')
+        return data['password2']
